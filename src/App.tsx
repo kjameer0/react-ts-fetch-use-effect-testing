9aa943c1aa71146e368d13f4e0c31a1f3ea95498
CodeSandbox <support@codesandbox.io>
2023-01-24 12:19:13 +0000

    "Initial commit"

---
+++ src/App.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+("./App.css");
+async function getLastCommitDate(username: string, repo_name: string) {
+  let last_commit;
+  const repo_url = `https://api.github.com/repos/${username}/${repo_name}`;
+  try {
+    const response = await fetch(repo_url);
+    if (!response.ok) {
+      throw new Error("hold this L");
+    }
+    const data = await response.json();
+    last_commit = data.pushed_at;
+  } catch (error) {
+    console.error(error);
+  } finally {
+    return last_commit || "no data";
+  }
+}
+function App() {
+  const [d, setD] = useState("");
+  useEffect(() => {
+    let ignore = false;
+    getLastCommitDate("kjameer0", "hashmap-visualizer").then((data) => {
+      if (!ignore) {
+        setD(data);
+      }
+    });
+    return () => {
+      ignore = true;
+    };
+  }, []);
+
+  return (
+    <div className="App">
+      <div className="project-display">
+        <h2>Data Structure Visualizer</h2>
+        <img
+          src="https://kjameer0.github.io/hashmap-visualizer/assets/binary-tree-5dde4aad.png"
+          alt="data-Structure-visualizer"
+        />
+        <p>{d}</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
